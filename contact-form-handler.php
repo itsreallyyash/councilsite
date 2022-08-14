1c5cdd255b45224a37731430a06ed5a552d03e6e
YashShah <89576926+itsreallyyash@users.noreply.github.com>
2022-08-14 15:19:02 +0530

    contact form

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,29 +1,17 @@
 <?php
-    $name = $_POST['name'];
-    $visitor_email = $_POST['email'];
-    $subject = $_POST['subject'];
-    $message = $_POST['message'];
+//get data from form  
 
-
-    $email_from = 'yash.shah033@nmims,edu.in';
-
-    $email_subject = "New Form Submission";
-    
-    $email_body =   "User Name: $name.\n".
-                    "User Email: $visitor_email.\n".
-                    "Subject: $subject.\n".
-                    "User Message: $message.\n";
-
-
-    $to = "yash12628@gmail.com";
-
-    $headers = "From: $email_from \r\n";
-
-    $headers .= "Reply-To: $visitor_email \r\n";
-
-    mail($to,$email_subject,$email_body,$headers);
-
-    header("Location: contact.html");
-
-
+$name = $_POST['name'];
+$email= $_POST['email'];
+$message= $_POST['message'];
+$to = "yash.shah033@nmims.edu.in";
+$subject = "Mail From website";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+$headers = "From: noreply@yoursite.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:blog.html");
 ?>
