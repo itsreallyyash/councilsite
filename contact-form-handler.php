ce3a7a97afbd83235755d01d3ed6e55ddaae2264
Prabir Kalwani <prabir.kalwani@gmail.com>
2023-02-15 13:57:24 +0530

    ->fixed navbar -> made responsive ->change background color ->change timeline
    issues :
    ->fix contact form page
    ->change about us page

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -4,7 +4,7 @@
 $name = $_POST['name'];
 $email= $_POST['email'];
 $message= $_POST['message'];
-$to = "yash.shah033@nmims.edu.in";
+$to = "studentscouncilmpstme@gmail.com";
 $subject = "Mail From website";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
 $headers = "From: noreply@yoursite.com" . "\r\n" .
